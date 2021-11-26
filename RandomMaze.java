cb389633bc283149c52475c37ee8dbfb499db71e
kennguyen111 <kennguyen111usa@gmail.com>
2021-11-26 00:30:38 -0500

    Added recursion to the maze

--- RandomMaze.java
+++ RandomMaze.java
@@ -3,62 +3,123 @@
  author Ken Nguyen
  version 1.0
  */
-import java.util.Random;
 
-public class RandomMaze{
-    //standard console size in characters.
-    private static final int HEIGHT = 25;
-    private static final int WIDTH = 80;
 
-    private static final char ICON_WALL = '#';
-    private static final char ICON_BLANK = ' ';
+public class RandomMaze {
 
+    private static final int WIDTH = 100;
+    private static final int HEIGHT = 30;
+
+    private static final char WALL = '#';
+    private static final char BLANK = ' ';
 
 
     /**
-     * Create a maze frame
+     * Create a frame of maze
+     *
      * @return 2D array of maze frame
      */
     private static char[][] createFrame() {
 
-        char [][] frame= new char[HEIGHT][WIDTH];
+        char[][] frame = new char[HEIGHT][WIDTH];
 
         //create a blank frame
-        for(int i=0; i< HEIGHT; i++){
-            for (int j=0; j< WIDTH; j++){
-                frame[i][j]= ICON_BLANK;
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                frame[i][j] = BLANK;
             }
         }
 
-
         //left column
-        for (int i=0; i <  HEIGHT; i++){
-            frame[i][0]= ICON_WALL;
+        for (int i = 0; i < HEIGHT; i++) {
+            frame[i][0] = WALL;
         }
 
         //Right column
-        for (int j=0; j < HEIGHT; j++){
-            frame[j][WIDTH-1]= ICON_WALL;
+        for (int j = 0; j < HEIGHT; j++) {
+            frame[j][WIDTH - 1] = WALL;
         }
 
         //Top column
-        for (int k =0 ; k <WIDTH; k++){
-            frame[0][k]=ICON_WALL;
+        for (int k = 0; k < WIDTH; k++) {
+            frame[0][k] = WALL;
         }
         //Bottom column
-        for (int b=0; b<WIDTH; b++){
-            frame[HEIGHT-1][b]=ICON_WALL;
+        for (int b = 0; b < WIDTH; b++) {
+            frame[HEIGHT - 1][b] = WALL;
         }
-
-
         return frame;
     }
 
+
+    /**
+     * generate a random number
+     *
+     * @param min
+     * @param max
+     * @return: a random number
+     */
+    private static int randBetween(int min, int max) {
+
+        int random = (int) (Math.random() * ((max - min))) + min;
+        return random;
+    }
+
+
     /**
      * recursion inside the maze
-
+     * parameter: 2d array
      */
 
+    private static void makeMazeRecursive(char[][] level, int startX, int startY, int endX, int endY) {
+        // base cases
+        if ((endX - startX) <=1 )  {
+            return;
+        }
+        if ((endY - startY) <= 1)  {
+            return;
+        }
+
+        // random new X and new y coordinates
+       int newY = randBetween(startY+1, endY-1 );
+       int newX  = randBetween(startX+1, endX-1 );
+
+        //create new column
+        for (int i = startY; i <= endY; i++) {
+            level[i][newX] = WALL;
+        }
+        level[randBetween(startY, newY-1)][newX] = BLANK; //random hole from startY to newY (upper)
+        level[randBetween(newY-1, endY)][newX] = BLANK; //random hole from newY to endY (lower)
+
+        //create new row
+        for (int j = startX; j <= endX; j++) {
+            level[newY][j] = WALL;
+        }
+        level[newY][randBetween(startX, newX-1)] = BLANK; // random hole from startX to new X (Left)
+        level[newY][randBetween(newX, endX)] = BLANK; // random hole from newX to endX (Right)
+
+
+        // call each sub-areas using recursion(total 4 areas)
+        makeMazeRecursive(level, startX+1, startY+1, newX-1, newY-1);  //upper left part
+        makeMazeRecursive(level, newX+1, startY+1 , endX-1  , newY-1); //upper right part
+        makeMazeRecursive(level, startX+1, newY+1, newX-1, endY-1); //lower left part
+        makeMazeRecursive(level, newX+1, newY+1, endX-1, endY-1); //lower right part
+
+
+    }
+
+
+
+    /**
+     * create a random maze
+     */
+    private static char[][] createRandomMaze(){
+        //get frame for random Maze
+        char[][] randomMaze =createFrame();
+        //make random maze
+        makeMazeRecursive(randomMaze,1,1,WIDTH-2, HEIGHT-2);
+        return randomMaze;
+    }
 
     private static void displayMaze(char[][] maze){
 
@@ -76,8 +137,8 @@ public class RandomMaze{
      * @param args command line arguments
      */
     public static void main(String[] args) {
-        displayMaze(createFrame());
-        //displayMaze(createBlankLevel());
+        displayMaze(createRandomMaze());
+
     }
 }
 
